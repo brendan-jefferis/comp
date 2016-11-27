a584e253adc88358e2ca43b22bd05a3892a0b471
brendan-jefferis <brendan.jefferis@gmail.com>
2016-11-27 21:51:43 +1300

    Completed recorder refactor

--- componentizer-recorder.js
+++ componentizer-recorder.js
@@ -28,12 +28,16 @@ Recorder.Actions = (model) => {
 		},
 		save: () => {
 			let now = new Date();
-	        let storageId = `${this.sessionName}-${now.getDay()}${now.getMonth()}${now.getYear()}${now.getHours()}${now.getMinutes()}${now.getSeconds()}`;
-	        localStorage.setItem(storageId, JSON.stringify(this));
-	        console.log(storageId);
+	        let recordingId = `${model.sessionName}-${now.getDay()}${now.getMonth()}${now.getYear()}${now.getHours()}${now.getMinutes()}${now.getSeconds()}`;
+	        localStorage.setItem(recordingId, JSON.stringify(model));
+	        console.log(recordingId);
 		},
 		load: (id) => {
 			let loadedState = JSON.parse(localStorage.getItem(id));
+	        if (loadedState === null) {
+	        	console.log("Failed to load recording. Please check your recording id and try again.");
+	        	return;
+	        }
 	        model.steps = loadedState.steps;
 	        Object.keys(loadedState.components).map((componentName) => {
 	            model.components[componentName].initialState = loadedState.components[componentName].initialState;
@@ -43,14 +47,34 @@ Recorder.Actions = (model) => {
 		storeComponent: (component, componentModel) => {
 			model.components[component.componentName] = component;
         	model.components[component.componentName].initialState = Object.assign({}, componentModel);
+		},
+		setRecordingId: (recordingId) => {
+			model.recordingId = recordingId;
 		}
 	};
 };
 
 
 Recorder.View = () => {
+	const COMPONENT = $("[data-component=componentizer-recorder]");
+	const BUTTON_PAUSE = COMPONENT.find("[data-selector=recorder-pause]");
+	const BUTTON_RESUME = COMPONENT.find("[data-selector=recorder-resume]");
+	const BUTTON_SAVE = COMPONENT.find("[data-selector=recorder-save]");
+	const BUTTON_LOAD = COMPONENT.find("[data-selector=recorder-load]");
+	const BUTTON_REPLAY = COMPONENT.find("[data-selector=recorder-replay]");
+	const INPUT_RECORDING_ID = COMPONENT.find("[data-selector=recorder-recording-id]");
+
 	return {
-		init: (actions, model) => {},
-		render: (model) => {}
+		init: (actions, model) => {
+			BUTTON_PAUSE.on("click", actions.pause);
+			BUTTON_RESUME.on("click", actions.resume);
+			BUTTON_SAVE.on("click", actions.save);
+			BUTTON_LOAD.on("click", () => { actions.load(model.recordingId); });
+			BUTTON_REPLAY.on("click", actions.replay);
+			INPUT_RECORDING_ID.on("keyup", (e) => { actions.setRecordingId(e.currentTarget.value); });
+		},
+		render: (model) => {
+
+		}
 	};
 };
