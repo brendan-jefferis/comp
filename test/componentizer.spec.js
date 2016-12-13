80709fabcf028eedf4e3a2372c78ad8083c9e96b
brendan-jefferis <brendan.jefferis@gmail.com>
2016-12-14 07:53:49 +1300

    updated tools and added test outline

---
+++ test/componentizer.spec.js
@@ -0,0 +1,9 @@
+import test from "ava";
+
+test.todo("Should throw error if no component name supplied");
+test.todo("Should throw error if no Actions supplied");
+test.todo("Should have public actions");
+test.todo("Should have property get method");
+test.todo("View.init should be called if present");
+test.todo("View.render should be called if present");
+test.todo("Should call View.render after async action complete");
