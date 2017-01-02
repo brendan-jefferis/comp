ca08fe38b2399f348988ee6dcfc70b491fe80d95
tv-brendan-jefferis <brendan@tradevine.com>
2017-01-02 18:23:37 +1300

    Bumped version

--- comp.js
+++ comp.js
@@ -1,5 +1,5 @@
 /* ____ ____ _  _ ___   
-*  |___ [__] |\/| |--' . v1.1.1
+*  |___ [__] |\/| |--' . v1.2.0
 * 
 * A design pattern and micro-framework for creating UI components
 *
@@ -8,7 +8,7 @@
 * 
 * Issues? Please visit https://github.com/brendan-jefferis/comp/issues
 *
-* Date: 2017-01-02T05:19:58.860Z 
+* Date: 2017-01-02T05:23:18.349Z 
 */
 (function (global, factory) {
 	typeof exports === 'object' && typeof module !== 'undefined' ? module.exports = factory() :
