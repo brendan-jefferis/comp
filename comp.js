967243f0725966d83c51b8670f5f119075708934
brendan-jefferis <brendan.jefferis@gmail.com>
2017-03-27 23:32:24 +1300

    Updated version

--- comp.js
+++ comp.js
@@ -1,5 +1,5 @@
 /* ____ ____ _  _ ___   
-*  |___ [__] |\/| |--' . v1.7.0
+*  |___ [__] |\/| |--' . v1.8.0
 * 
 * A design pattern and micro-framework for creating UI components
 *
@@ -8,7 +8,7 @@
 * 
 * Issues? Please visit https://github.com/brendan-jefferis/comp/issues
 *
-* Date: 2017-03-27T10:03:45.809Z 
+* Date: 2017-03-27T10:31:55.612Z 
 */
 (function (global, factory) {
 	typeof exports === 'object' && typeof module !== 'undefined' ? module.exports = factory() :
